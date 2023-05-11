73401447334cab241ab8abff88042b17f8151eec
Siris <40269790+Siris01@users.noreply.github.com>
2023-05-11 05:56:08 +0000

    save

--- src/build.ts
+++ src/build.ts
@@ -1,15 +1,20 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import template, { SchemeName } from './template'
+import borderLesstemplate from './borderless'
 
-const filePath = (variant: string, bordered: boolean) =>
-  path.join(process.cwd(), `/catppuccin-${variant}${bordered ? '-bordered' : ''}.json`)
+const filePath = (variant: string, ext?: string) =>
+  path.join(process.cwd(), `/catppuccin-${variant}${ext ? '-bordered' : ''}.json`);
+  
 
-;['latte', 'frappe', 'macchiato', 'mocha'].map((variant: SchemeName) => {
+['latte', 'frappe', 'macchiato', 'mocha'].map((variant: SchemeName) => {
   const bordered = JSON.stringify(template(variant, true), null, '\t')
-  const nonBordered = JSON.stringify(template(variant, false), null, '\t')
+  const normal = JSON.stringify(template(variant, false), null, '\t')
+  const borderless = JSON.stringify(borderLesstemplate(variant), null, '\t')
 
-  fs.writeFileSync(filePath(variant, true), bordered)
-  fs.writeFileSync(filePath(variant, false), nonBordered)
+  fs.writeFileSync(filePath(variant), normal)
+  fs.writeFileSync(filePath(variant, 'bordered'), bordered)
+  fs.writeFileSync(filePath(variant, 'borderless'), borderless)
+
   console.log(`Updated ${variant}`)
 })
